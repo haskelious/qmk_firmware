b065d07e60055aac9781d07070791183abb03aab
Stephane <stephane@frozenkiwi.net>
2023-12-22 19:21:41 +0100

    US intl v2

--- keyboards/crkbd/keymaps/haskelious/rules.mk
+++ keyboards/crkbd/keymaps/haskelious/rules.mk
@@ -1,6 +1,7 @@
 MOUSEKEY_ENABLE = yes    # Mouse keys
-RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
 OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
+#OLED_DRIVER     = SSD1306
 LTO_ENABLE      = yes
-VIA_ENABLE		= yes
+VIA_ENABLE		= no
+EXTRAFLAGS      += -flto
